ba1ae2c56a636b95706f57fbcc5bc1e526ed8414
XXXELUS1 <61873976+XXXELUS1@users.noreply.github.com>
2025-07-09 17:53:23 +0600

    Delete bot directory

--- bot/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-BACKEND_URL    = os.getenv("BACKEND_URL")
-WEBAPP_URL     = os.getenv("WEBAPP_URL")
-if not TELEGRAM_TOKEN or not BACKEND_URL or not WEBAPP_URL:
-    raise RuntimeError("TELEGRAM_TOKEN и API_BASE должны быть заданы в .env")
--- bot/handlers/id.py
+++
@@ -1,6 +0,0 @@
-from telegram import Update
-from telegram.ext import ContextTypes
-
-async def whoami(update: Update, ctx: ContextTypes.DEFAULT_TYPE):
-    uid = update.effective_user.id
-    await update.message.reply_text(f"Ваш Telegram ID: {uid}")
